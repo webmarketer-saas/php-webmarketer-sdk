e11c2820cf8a45437c64d84a3f8516c0a8d68533
Clement Cloux <clement.cloux@yabawt.com>
2021-10-08 17:03:30 +0200

    :bug: event metrics fix date when null

--- src/Api/Project/Events/EventService.php
+++ src/Api/Project/Events/EventService.php
@@ -34,12 +34,15 @@ class EventService extends ServiceWrapper
     public function upsertEventState($event_id, $storage_key, $value, $date = null, $config = [])
     {
         $project_id = $this->getProjectId($config);
+        $payload = ["value" => $value];
+        if (!is_null($date)) {
+            $payload["date"] = $date;
+        }
+
+
         $this->api_service->patch(
             "projects/$project_id/events/$event_id/states/$storage_key",
-            [
-                "value" => $value,
-                "date" => $date,
-            ]
+            $payload
         );
     }
 
@@ -54,12 +57,14 @@ class EventService extends ServiceWrapper
     public function upsertEventStatistic($event_id, $storage_key, $value, $date = null, $config = [])
     {
         $project_id = $this->getProjectId($config);
+        $payload = ["rawValue" => $value];
+        if (!is_null($date)) {
+            $payload["date"] = $date;
+        }
+
         $this->api_service->patch(
             "projects/$project_id/events/$event_id/statistics/$storage_key",
-            [
-                "rawValue" => $value,
-                "date" => $date,
-            ]
+            $payload
         );
     }
 }
